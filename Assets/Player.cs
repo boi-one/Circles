2ffb16f4eec393eaef0db8bae0d4a0a94d1da252
unknown <30570@ma-web.nl>
2022-05-02 22:11:33 +0200

    muis

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -47,12 +47,16 @@ public class Player : MonoBehaviour
         mousePos = Camera.main.ScreenToWorldPoint(mousePos); //kijkt waar muis is
         Vector2 Direction = new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y); //draait de object waar dit script op zit
         transform.up = Direction; //weet niet waarom dit up is maar alles word fucked als je position gebruik
+        Debug.Log("X " + mousePos.x + "Y " + mousePos.y);
     }
     public void Shoot()
     {
         Vector3 mousePos = Input.mousePosition;
+        //mousePos = Camera.main.ScreenToWorldPoint(mousePos);
         //fire = false
         GameObject bullet = Instantiate(bulletprefab, firepoint.position, firepoint.rotation);
-        bullet.GetComponent<BulletMovement>().targetDir = new Vector2(mousePos.x, mousePos.y);  
+        Debug.Log("de bullet instantiate");
+        bullet.GetComponent<BulletMovement>().targetDir = mousePos;
+        Debug.Log("de bullet schiet");
     }
 }
