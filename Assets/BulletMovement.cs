a5c1e8eee94bffe683d2e816efa8887409fed268
unknown <30570@ma-web.nl>
2022-05-02 18:03:02 +0200

    schieten bijna af

--- Assets/BulletMovement.cs
+++ Assets/BulletMovement.cs
@@ -6,11 +6,10 @@ public class BulletMovement : MonoBehaviour
 {
     public Vector3 targetDir;
     public float speed;
-    public int distance;
-    public float radius;
+
     // Update is called once per frame
     void Update()
     {
-        transform.position += targetDir * 1 / 60 * speed;
+        transform.position += targetDir * speed;
     }
 }
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,18 +5,14 @@ using System.Linq;
 
 public class Player : MonoBehaviour
 {
-    public GameObject bullet;
+    public GameObject bulletprefab;
     public Transform firepoint;
     public float speed;
-    float lastShot;
-    public float cooldown;
-
-    bool bruh = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -24,14 +20,7 @@ public class Player : MonoBehaviour
     {
         if (Input.GetMouseButton(0))
         {
-            if (Time.time - lastShot < cooldown)
-            {
-                return;
-            }
-            else 
-            {
-                GameObject Bullet = Instantiate(bullet, firepoint.position, firepoint.rotation);
-            }
+            Shoot();
         }
 
         if (Input.GetKey(KeyCode.A))
@@ -57,6 +46,13 @@ public class Player : MonoBehaviour
         Vector3 mousePos = Input.mousePosition;
         mousePos = Camera.main.ScreenToWorldPoint(mousePos); //kijkt waar muis is
         Vector2 Direction = new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y); //draait de object waar dit script op zit
-        transform.up = Direction; //weet niet waarom dit up is maar alles word fucked als je position gebruikt
+        transform.up = Direction; //weet niet waarom dit up is maar alles word fucked als je position gebruik
+    }
+    public void Shoot()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        //fire = false
+        GameObject bullet = Instantiate(bulletprefab, firepoint.position, firepoint.rotation);
+        bullet.GetComponent<BulletMovement>().targetDir = new Vector2(mousePos.x, mousePos.y);  
     }
 }
